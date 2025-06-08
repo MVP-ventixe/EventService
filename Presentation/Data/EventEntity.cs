fd973af9e7ec868a402e15d517ffca25e426e709
ThorowZ <dennisgranatdennisgranat@hotmail.com>
2025-06-08 02:20:18 +0200

    init

---
+++ Presentation/Data/EventEntity.cs
@@ -0,0 +1,12 @@
+﻿namespace Presentation.Data;
+
+public class EventEntity
+{
+    public int Id { get; set; }
+    public string Name { get; set; } = string.Empty;
+    public DateTime Date { get; set; }
+    public string Location { get; set; } = string.Empty;
+    public string Description { get; set; } = string.Empty;
+    public bool IsActive { get; set; } = true;
+
+}
--- Presentation/Program.cs
+++ Presentation/Program.cs
@@ -1,21 +1,20 @@
+using Microsoft.EntityFrameworkCore;
+using Presentation.Data;
+using Presentation.Services;
+
 var builder = WebApplication.CreateBuilder(args);
-
-
-
 builder.Services.AddControllers();
-
 builder.Services.AddOpenApi();
+builder.Services.AddScoped<EventService>();
+builder.Services.AddDbContext<DataContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("EventDataBaseConnection")));
 
 var app = builder.Build();
 
 
-if (app.Environment.IsDevelopment())
-{
-
-}
-
+app.MapOpenApi();
 app.UseHttpsRedirection();
-
+app.UseCors(x => x.AllowAnyOrigin().AllowAnyHeader().AllowAnyMethod());
 app.UseAuthentication();
 app.UseAuthorization();
 
